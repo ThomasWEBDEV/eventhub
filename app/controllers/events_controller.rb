4a05e53bafe4a448be791868c58da17483af8ef3
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-06 11:43:38 +0200

    implement controllers with crud methods and related views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,30 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = current_user.events.new
   end
 
   def create
+    @event = current_user.events.new(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Événement créé !'
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
-
-  def destroy
+  def event_params
+    params.require(:event).permit(:title, :description, :start_date, :location, :max_participants)
   end
 end
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,7 +1,16 @@
 class ParticipationsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @event = Event.find(params[:event_id])
+    @participation = @event.participations.create(user: current_user, status: 'confirmed')
+    redirect_to @event, notice: 'Inscription confirmée !'
   end
 
   def destroy
+    @participation = Participation.find(params[:id])
+    @event = @participation.event
+    @participation.destroy
+    redirect_to @event, notice: 'Inscription annulée.'
   end
 end
