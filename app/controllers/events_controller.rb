5df5348b3ed5d6c43d67a78060d15006b0a17052
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-06 14:48:24 +0200

    Add delete functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path, notice: 'Événement supprimé !'
+  end
+
   private
 
   def event_params
